ad6b4ec43b5256c535d72371c7cd1d7229337956
Dmitry <143950664+lowkey-dy@users.noreply.github.com>
2023-12-18 19:10:04 +0700

    practice 3 done

--- 28 methods and properties of strings and numbers/script.js
+++ 28 methods and properties of strings and numbers/script.js
@@ -1,2 +1,40 @@
 'use strict';
 
+const str = "teSt";
+const arr = [1, 2, 3, 4];
+
+// console.log(str[2]);
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+// console.log(str);
+
+let fruit = "Some fruit";
+
+console.log(fruit.indexOf("fruit"));
+
+const logg = "Hello world";
+
+console.log(logg.slice(6, 11));
+
+console.log(logg.substring(6, 11));
+
+console.log(logg.substr(6, 11));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = '12.2px';
+
+console.log(parseInt(test));
+console.log(parseFloat(test));
+
+
+
+
+
+
+
+
+
+
+
---
+++ 29 Practice part_3 Function uses/script.js
@@ -0,0 +1,83 @@
+'use strict';
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
+
+let numberOfFilms;
+
+function start () {
+    numberOfFilms = +prompt('How many movies are you whatch?', '');
+
+     while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('How many movies are you whatch?', '');
+     }
+}
+start();
+
+const personalMovieDB = {
+    count:numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false
+};
+
+// console.log(personalMovieDB.count);
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('One of the last movie you watched?'),
+              b = +prompt('How much do you rate it?');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Watched quite a few films!');
+    
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('You a classic viewer!');
+    
+    } else if (personalMovieDB.count >= 30) {
+        console.log('You are a film buff');
+    } else {
+        console.log('Error');
+    }
+}
+detectPersonalLevel();
+
+function showMyDB(hiden) {
+    if (!hiden) {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Your favorite genre is number ${i}`);
+    }
+}
+writeYourGenres();
+
+
+
+ 

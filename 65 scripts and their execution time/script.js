d164ffe7a33b2b6044e041e9ba02338611b91343
Dmitry <143950664+lowkey-dy@users.noreply.github.com>
2023-12-25 21:36:34 +0700

    timeInterval

---
+++ 65 scripts and their execution time/script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+// const timerId = setTimeout(function() {
+//     console.log('Hello');
+// }, 2000);
+
+const btn = document.querySelector('.btn');
+let timerId,
+    i = 0;
+
+function myAnimation() {
+    const elem = document.querySelector('.box');
+    let pos = 0;
+
+    const id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 300) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + "px";
+            elem.style.left = pos + "px";
+        }
+    }
+}
+
+btn.addEventListener('click', myAnimation);
+
+// function logger () {
+//     if (i === 3) {
+//         clearInterval(timerId);
+//     }
+//     console.log('Hello');
+//     i++;
+// }
+
+// const id = setTimeout(function log () {
+//     console.log('Hilooo');
+//     // id = setTimeout(log, 500);
+// }, 500)

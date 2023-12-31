25df01fc33a887359857f0dc94b82fa37d40851f
Dmitry <143950664+lowkey-dy@users.noreply.github.com>
2023-12-31 14:32:46 +0700

    ajax

--- AJAX and communication with server/js/script.js
+++ AJAX and communication with server/js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const inputRub = document.querySelector('#rub'),
+      inputUsd = document.querySelector('#usd');
+
+
+inputRub.addEventListener('input', () => {
+    const request = XMLHttpRequest();
+
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'aplication/json; charset=utf-8');
+    request.send();
+
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState === 4 && request.status === 200) {
+            console.log(request.response);
+        }
+    });
+});

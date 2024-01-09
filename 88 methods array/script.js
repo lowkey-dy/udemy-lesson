ce714d07cd22762dc499eb414d0c99873d7d235c
Dmitry <143950664+lowkey-dy@users.noreply.github.com>
2024-01-09 19:15:59 +0700

    Create script.js

---
+++ 88 methods array/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+// const names = ['Ivan', 'Dima', 'Archibald', 'Srachibald'];
+
+// const shortNames = names.filter(function(name) {
+//     return name.length < 5;
+// });
+// console.log(shortNames);
+
+let answers = ['Bill', 'DrIll', 'AvToNdil, GOMODRILL'];
+
+answers = answers.map(item => item.toLowerCase());
+
+console.log(answers);

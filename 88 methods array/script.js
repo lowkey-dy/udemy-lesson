75e5d084a48b8808dd57a27e0daab4e3d73c6552
Dmitry <143950664+lowkey-dy@users.noreply.github.com>
2024-01-13 13:05:59 +0700

    Update script.js

--- 88 methods array/script.js
+++ 88 methods array/script.js
@@ -22,3 +22,27 @@ const some = [4, 7, 9];
 
 console.log(some.every(item => typeof(item) === 'number'));
 
+//reduce
+
+// const arr = [4, 5, 7, 21];
+
+// const res = arr.reduce((sum, current) => sum + current, 3);
+// console.log(res);
+
+const arr = ['apple', 'plum', 'srum'];
+
+const res = arr.reduce((sum, current) => `${sum}, ${current}`);
+console.log(res);
+
+const obj = {
+    ivan: 'persone',
+    ann: 'persone',
+    dog: 'animal',
+    cat: 'animal'
+};
+
+const newArr = Object.entries(obj)
+.filter(item => item[1] === 'persone')
+.map(item => item[0]);
+
+console.log(newArr);
